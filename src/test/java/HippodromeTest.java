e984795211dd78eafdb4470951f637fac2b70438
Vassiliy Bayev <bayev.v@gmail.com>
2024-12-30 11:53:26 +0500

    Created three tests getHorses, move, getWinner.

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,9 +1,11 @@
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class HippodromeTest {
 
@@ -23,5 +25,43 @@ public class HippodromeTest {
                 assertThrows(IllegalArgumentException.class, () -> new Hippodrome(new ArrayList<>()));
 
         assertEquals("Horses cannot be empty.", exception.getMessage());
+    }
+
+    @Test
+    public void getHorses() {
+
+        List<Horse> horses = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            horses.add(new Horse("horse " + i, i, i ));
+        }
+        Hippodrome horse = new Hippodrome(horses);
+
+        assertEquals(horses, horse.getHorses());
+    }
+
+    @Test
+    public void move() {
+
+        List<Horse> horses = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            horses.add(mock(Horse.class));
+        }
+        new Hippodrome(horses).move();
+
+        for (Horse horse : horses) {
+            verify(horse).move();
+        }
+    }
+
+    @Test
+    public void getWinner() {
+
+        Horse horse1 = new Horse("Horse1", 1, 1);
+        Horse horse2 = new Horse("Horse2", 2, 2);
+        Horse horse3 = new Horse("Horse3", 3, 3);
+        Horse horse4 = new Horse("Horse4", 4, 4);
+        Hippodrome hippodrome = new Hippodrome(List.of(horse1, horse2, horse3, horse4));
+
+        assertSame(horse4, hippodrome.getWinner());
     }
 }
